738d2d566db5f34cb3fe64802cf5d44b3440b5c6
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-03-09 02:52:45 +0530

    setup with mongodb

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hi this is twitter app");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("mongodb is connected ");
+  })
+  .catch((error) => {
+    console.log(error, " we got server error");
+  });
+const app = express();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
