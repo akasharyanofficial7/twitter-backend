08720ee713b792d70a0f1946d7a68b466be3e3c9
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-03-14 09:45:25 +0530

    added controller and api for create tweet

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import Tweet from "./src/models/tweet.js";
-// import { TweetRepository, HashtagRepository } from "./src/repository/index.js";
-import TweetService from "./src/services/tweet-services.js";
+import service from "./src/services/tweet-services.js";
+import apiRoutes from "./src/routes/index.js";
+import bodyParser from "body-parser";
 
 dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", apiRoutes);
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -19,13 +25,9 @@ mongoose
     console.log(error, " we got server error");
   });
 
-const app = express();
-
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
-
-  const service = new TweetService();
-  const tweet = await service.create({
-    content: "this is  a my first  #BOOK BANK  aur sare #DOST ",
-  });
 });
+
+// let response = new service();
+// await response.create({ content: "this is a #book " });
